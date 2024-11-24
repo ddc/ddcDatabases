2e32965db8736d732190c88c267caafc3a4f515d
ddc <ddc@users.noreply.github.com>
2024-11-24 17:25:52 -0300

    updated utils

--- ddcDatabases/db_utils.py
+++ ddcDatabases/db_utils.py
@@ -1,10 +1,13 @@
 # -*- encoding: utf-8 -*-
+from sqlalchemy.engine.result import MappingResult
+from sqlalchemy.ext.asyncio import AsyncMappingResult
 from .exceptions import (
-    DBAddException,
+    DBDeleteAllDataException,
     DBExecuteException,
     DBFetchAllException,
-    DBFetchOneException,
-    DBFetchValueException
+    DBFetchValueException,
+    DBInsertBulkException,
+    DBInsertSingleException
 )
 
 
@@ -12,23 +15,7 @@ class DBUtils:
     def __init__(self, session):
         self.session = session
 
-    def add(self, stmt):
-        try:
-            self.session.add(stmt)
-            self.session.commit()
-        except Exception as e:
-            self.session.rollback()
-            raise DBAddException(e)
-
-    def execute(self, stmt):
-        try:
-            self.session.execute(stmt)
-            self.session.commit()
-        except Exception as e:
-            self.session.rollback()
-            raise DBExecuteException(e)
-
-    def fetchall(self, stmt):
+    def fetchall(self, stmt) -> MappingResult:
         cursor = None
         try:
             cursor = self.session.execute(stmt)
@@ -39,50 +26,60 @@ class DBUtils:
         finally:
             cursor.close() if cursor is not None else None
 
-    def fetchone(self, stmt):
+    def fetchvalue(self, stmt) -> str | None:
         cursor = None
         try:
             cursor = self.session.execute(stmt)
-            return cursor.mappings().first()
-        except Exception as e:
-            self.session.rollback()
-            raise DBFetchOneException(e)
-        finally:
-            cursor.close() if cursor is not None else None
-
-    def fetch_value(self, stmt):
-        cursor = None
-        try:
-            cursor = self.session.execute(stmt)
-            return cursor.first()[0]
+            result = cursor.fetchone()
+            return str(result[0]) if result is not None else None
         except Exception as e:
             self.session.rollback()
             raise DBFetchValueException(e)
         finally:
             cursor.close() if cursor is not None else None
+
+    def insert(self, stmt) -> None:
+        try:
+            self.session.add(stmt)
+        except Exception as e:
+            self.session.rollback()
+            raise DBInsertSingleException(e)
+        finally:
+            self.session.commit()
+
+    def insertbulk(self, model, list_data: list[dict]) -> None:
+        try:
+            self.session.bulk_insert_mappings(model, list_data)
+        except Exception as e:
+            self.session.rollback()
+            raise DBInsertBulkException(e)
+        finally:
+            self.session.commit()
+
+    def deleteall(self, model) -> None:
+        try:
+            self.session.query(model).delete()
+        except Exception as e:
+            self.session.rollback()
+            raise DBDeleteAllDataException(e)
+        finally:
+            self.session.commit()
+
+    def execute(self, stmt) -> None:
+        try:
+            self.session.execute(stmt)
+        except Exception as e:
+            self.session.rollback()
+            raise DBExecuteException(e)
+        finally:
+            self.session.commit()
 
 
 class DBUtilsAsync:
     def __init__(self, session):
         self.session = session
 
-    async def add(self, stmt):
-        try:
-            self.session.add(stmt)
-            await self.session.commit()
-        except Exception as e:
-            self.session.rollback()
-            raise DBAddException(e)
-
-    async def execute(self, stmt):
-        try:
-            await self.session.execute(stmt)
-            await self.session.commit()
-        except Exception as e:
-            await self.session.rollback()
-            raise DBExecuteException(e)
-
-    async def fetchall(self, stmt):
+    async def fetchall(self, stmt) -> AsyncMappingResult:
         cursor = None
         try:
             cursor = await self.session.execute(stmt)
@@ -93,24 +90,50 @@ class DBUtilsAsync:
         finally:
             cursor.close() if cursor is not None else None
 
-    async def fetchone(self, stmt):
+    async def fetchvalue(self, stmt) -> str | None:
         cursor = None
         try:
             cursor = await self.session.execute(stmt)
-            return cursor.mappings().first()
+            result = cursor.fetchone()
+            return str(result[0]) if result is not None else None
         except Exception as e:
             await self.session.rollback()
-            raise DBFetchOneException(e)
-        finally:
-            cursor.close() if cursor is not None else None
-
-    async def fetch_value(self, stmt):
-        cursor = None
-        try:
-            cursor = await self.session.execute(stmt)
-            return cursor.first()[0]
-        except Exception as e:
-            self.session.rollback()
             raise DBFetchValueException(e)
         finally:
             cursor.close() if cursor is not None else None
+
+    async def insert(self, stmt) -> None:
+        try:
+            self.session.add(stmt)
+        except Exception as e:
+            await self.session.rollback()
+            raise DBInsertSingleException(e)
+        finally:
+            await self.session.commit()
+
+    async def insertbulk(self, model, list_data: list[dict]) -> None:
+        try:
+            self.session.bulk_insert_mappings(model, list_data)
+        except Exception as e:
+            await self.session.rollback()
+            raise DBInsertBulkException(e)
+        finally:
+            await self.session.commit()
+
+    async def deleteall(self, model) -> None:
+        try:
+            await self.session.query(model).delete()
+        except Exception as e:
+            await self.session.rollback()
+            raise DBDeleteAllDataException(e)
+        finally:
+            await self.session.commit()
+
+    async def execute(self, stmt) -> None:
+        try:
+            await self.session.execute(stmt)
+        except Exception as e:
+            await self.session.rollback()
+            raise DBExecuteException(e)
+        finally:
+            await self.session.commit()
